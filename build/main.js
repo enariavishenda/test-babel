a2c7a0d9032e8734566dddb48528b3793cbd0542
Enaria Vishenda <matthewfarrel@yandex.ru>
2020-08-10 13:48:49 +0500

    test polyfills & jsx

--- build/main.js
+++ build/main.js
@@ -1,15 +1,25 @@
-"use strict";
+import "core-js/modules/es.array.flat";
+import "core-js/modules/es.array.unscopables.flat";
+import "core-js/modules/es.promise";
 
 function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
 
-class App {
+import React from 'react';
+import ReactDom from 'react-dom';
+
+const App = () => /*#__PURE__*/React.createElement("p", null, "Hello");
+
+ReactDom.render( /*#__PURE__*/React.createElement(App, null), document.getElementById('root'));
+
+class OApp {
   constructor() {
     _defineProperty(this, "hello", async (name = 'Mike') => {
       console.log(`Hello ${name}`);
+      console.log([1, 2, [2, 3]].flat());
     });
   }
 
 }
 
-const app = new App();
-app.hello().then(() => console.log('done')).catch(() => console.log('error'));
+const oapp = new OApp();
+oapp.hello().then(() => console.log('done')).catch(() => console.log('error'));
--- src/main.js
+++ src/main.js
@@ -1,12 +1,20 @@
+import React from 'react';
+import ReactDom from 'react-dom'
 
-class App {
+const App = () => <p>Hello</p>
+
+ReactDom.render(<App />,
+    document.getElementById('root'))
+
+class OApp {
     hello = async (name = 'Mike') => {
         console.log(`Hello ${name}`)
+        console.log([1,2, [2, 3]].flat())
     }
 }
 
-const app = new App()
-app.hello()
+const oapp = new OApp()
+oapp.hello()
     .then(() => console.log('done'))
     .catch(() => console.log('error'))
 
